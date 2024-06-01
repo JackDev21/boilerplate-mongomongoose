baad3aa29f9f78b35fe729b37925c6bc96296b95
Jose A. Cantó (Jack) <jose.a.c.lopez@gmail.com>
2024-06-01 08:16:56 +0000

    fix my app and uri

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose")
 
 const MONGO_URI = process.env.MONGO_URI
 
+console.log("MongoDB URI:", MONGO_URI)
+
 mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 
